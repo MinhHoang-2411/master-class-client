ee1df5ce026438e7365566d53eddc77d220d18a5
nhat_minh <hnnminh24112000@gmail.com>
2023-03-15 12:43:21 +0700

    #create-sticky-footer-and-ui-payment-modal

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -4,10 +4,13 @@ import SendEmail from '@/contents/auth/ForgotPassword/SendEmail';
 import VerifyCode from '@/contents/auth/ForgotPassword/VerifyCode';
 import SignInModal from '@/contents/auth/SignInModal';
 import SignUpModal from '@/contents/auth/SignUpModal';
+import PaymentModal from '@/contents/home/PaymentModal';
 import { authActions } from '@/store/auth/authSlice';
+import { paymentActions } from '@/store/payment/paymentSlice';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { Box } from '@mui/material';
 import dynamic from 'next/dynamic';
+import StickyFooter from './StickyFooter';
 
 // const Footer = dynamic(() => import('./Footer'));
 const Navbar = dynamic(() => import('./Navbar'), {
@@ -24,13 +27,13 @@ export default function MyLayout({ children }: any) {
   const modalResetPassword = useAppSelector((state) => state.auth.modalResetPassword);
 
   const modalChangePassword = useAppSelector((state) => state.auth.modalChangePassword);
+  const modalChoosePayment = useAppSelector((state) => state.payment.modalChoosePayment);
 
   return (
     <Box sx={{ overflowX: 'hidden' }}>
       <Navbar />
       {children}
       {/* <Footer /> */}
-
       <SignInModal
         isOpen={modalSignIn.isOpen}
         CloseModal={() => dispatch(authActions.closeSignInModal())}
@@ -51,11 +54,21 @@ export default function MyLayout({ children }: any) {
         isOpen={modalResetPassword.isOpen}
         CloseModal={() => dispatch(authActions.closeModalResetPassword())}
       />
-      
+
       <ChangePassword
         isOpen={modalChangePassword.isOpen}
         CloseModal={() => dispatch(authActions.closeChangePassModal())}
       />
+
+      <PaymentModal
+        isOpen={modalChoosePayment.isOpen}
+        closeModal={() => {
+          localStorage.removeItem('openModalPayment');
+          dispatch(paymentActions.closeModalChoosePayment());
+        }}
+      />
+
+      <StickyFooter />
     </Box>
   );
 }
